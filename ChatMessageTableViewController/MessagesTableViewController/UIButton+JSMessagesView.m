a471f2020108f3721ccd4e174722550a8516d681
KazaiMazai <kazaimazai@gmail.com>
2014-07-31 19:21:17 +0400

    color error fix

--- ChatMessageTableViewController/MessagesTableViewController/UIButton+JSMessagesView.m
+++ ChatMessageTableViewController/MessagesTableViewController/UIButton+JSMessagesView.m
@@ -9,6 +9,8 @@
 #import "UIButton+JSMessagesView.h"
 #import "JSMessageInputView.h"
 
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0];;
+
 @implementation UIButton (JSMessagesView)
 
 + (UIButton *)defaultSendButton
